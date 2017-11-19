3df6496759a3fc9c45657010296c07f1f3305888
euangordon <32622054+euangordon@users.noreply.github.com>
2017-11-19 14:23:10 +0000

    initial upload

---
+++ MyImageRenderListener.cs
@@ -0,0 +1,115 @@
+﻿/*
+ * This class is part of the book "iText in Action - 2nd Edition"
+ * written by Bruno Lowagie (ISBN: 9781935182610)
+ * For more info, go to: http://itextpdf.com/examples/
+ * This example only works with the AGPL version of iText.
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace NautoShark.PDFStamper
+{
+    public class MyImageRenderListener : IRenderListener
+    {
+        // ===========================================================================
+        /** the byte array of the extracted images */
+        private float _imgWidth;
+        public float ImgWidth
+        {
+            get { return _imgWidth; }
+        }
+
+        private float _imgHeight;
+        public float ImgHeight
+        {
+            get { return _imgHeight; }
+        }
+
+        private float _ctmWidth;
+        public float CtmWidth
+        {
+            get { return _ctmWidth; }
+        }
+
+        private float _ctmHeight;
+        public float CtmHeight
+        {
+            get { return _ctmHeight; }
+        }
+
+        private float _xlocation;
+        public float Xlocation
+        {
+            get { return _xlocation; }
+        }
+
+        private float _ylocation;
+        public float Ylocation
+        {
+            get { return _ylocation; }
+        }
+
+        // ---------------------------------------------------------------------------
+        /**
+         * Creates a RenderListener that will look for images.
+         */
+        public MyImageRenderListener()
+        {
+            _imgWidth = new float();
+            _imgHeight = new float();
+            _ctmWidth = new float();
+            _ctmHeight = new float();
+            _xlocation = new float();
+            _ylocation = new float();
+        }
+        // ---------------------------------------------------------------------------
+        /**
+         * @see com.itextpdf.text.pdf.parser.RenderListener#beginTextBlock()
+         */
+        public void BeginTextBlock() { }
+        // ---------------------------------------------------------------------------     
+        /**
+         * @see com.itextpdf.text.pdf.parser.RenderListener#endTextBlock()
+         */
+        public void EndTextBlock() { }
+        // ---------------------------------------------------------------------------     
+        /**
+         * @see com.itextpdf.text.pdf.parser.RenderListener#renderImage(
+         *     com.itextpdf.text.pdf.parser.ImageRenderInfo)
+         */
+        public void RenderImage(ImageRenderInfo renderInfo)
+        {
+            try
+            {
+                var img = renderInfo.GetImage().GetDrawingImage();
+                _imgWidth = img.Width;
+                _imgHeight = img.Height;
+                img.Dispose();
+
+                //Get the current transformation matrix
+                var ctm = renderInfo.GetImageCTM();
+                _ctmWidth = ctm[Matrix.I12];
+                _ctmHeight = ctm[Matrix.I21];
+                _xlocation = ctm[Matrix.I31];
+                _ylocation = ctm[Matrix.I32];
+            }
+            catch
+            {
+                // pass through any other unsupported image types
+            }
+        }
+        // ---------------------------------------------------------------------------     
+        /**
+          * @see com.itextpdf.text.pdf.parser.RenderListener#renderText(
+          *     com.itextpdf.text.pdf.parser.TextRenderInfo)
+          */
+        public void RenderText(TextRenderInfo renderInfo) { }
+        // ===========================================================================
+    }
+}
---
+++ PDF.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.IO;
+
+namespace NautoShark.PDFStamper
+{
+    public class PDFStamper
+    {
+        public string UKHOVARLicenceNumber, UKHOCopyRightYear;
+        public PDFStamper(string ukhoVARLicenceNumber, string ukhoCopyRightYear)
+        {
+            //2484/170721/01
+            UKHOVARLicenceNumber = ukhoVARLicenceNumber;
+            //2017
+            UKHOCopyRightYear = ukhoCopyRightYear;
+        }
+
+        public MemoryStream AddVARLicenceBlocks(MemoryStream inputStream)
+        {
+            PdfReader reader = new PdfReader((byte[])inputStream.ToArray());
+
+            MemoryStream outputStream = new MemoryStream();
+
+            using (Document document = new Document(reader.GetPageSizeWithRotation(1), 0, 0, 0, 0))
+            {
+                using (PdfWriter writer = PdfWriter.GetInstance(document, outputStream))
+                {
+                    document.Open();
+
+                    PdfImportedPage importedPage = writer.GetImportedPage(reader, 1);
+
+                    var pageRotation = reader.GetPageRotation(1);
+                    var pageWidth = reader.GetPageSizeWithRotation(1).Width;
+                    var pageHeight = reader.GetPageSizeWithRotation(1).Height;
+                    var titlefont = FontFactory.GetFont(BaseFont.COURIER, 7, Font.NORMAL);
+                    var title1 = new Paragraph(20, "Reproduced from Admiralty digital Notices to Mariners by permission of the Controller of Her Majesty’s Stationery", titlefont);
+                    var title2 = new Paragraph("Office and the UK Hydrographic Office", titlefont);
+                    var title1and2 = new Paragraph(20, "Reproduced from Admiralty digital Notices to Mariners by permission of the Controller of Her Majesty’s Stationery Office and the UK Hydrographic Office", titlefont);
+                    var title3 = new Paragraph("HO " + UKHOVARLicenceNumber + " © British Crown Copyright " + UKHOCopyRightYear, titlefont);
+
+                    title1.Alignment = Element.ALIGN_RIGHT;
+                    title2.Alignment = Element.ALIGN_RIGHT;
+                    title1and2.Alignment = Element.ALIGN_RIGHT;
+                    title3.Alignment = Element.ALIGN_RIGHT;
+                    title1.IndentationRight = 15;
+                    title2.IndentationRight = 15;
+                    title1and2.IndentationRight = 15;
+                    title3.IndentationRight = 15;
+
+                    PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+                    MyImageRenderListener listener = new MyImageRenderListener();
+                    parser.ProcessContent(1, listener);
+                    var imgWidth = listener.ImgWidth;
+                    var imgHeight = listener.ImgHeight;
+                    var ctmWidth = listener.CtmWidth;
+                    var ctmHeight = listener.CtmHeight;
+                    var xlocation = listener.Xlocation;
+                    var ylocation = listener.Ylocation;
+
+                    switch (pageRotation)
+                    {
+                        case 0:
+                            document.Add(title1);
+                            document.Add(title2);
+                            document.Add(title3);
+                            writer.DirectContent.AddTemplate(importedPage, 1f, 0, 0, 1f, 15-xlocation, -ylocation + 15);
+                            break;
+
+                        case 90:
+                            document.Add(title1and2);
+                            document.Add(title3);
+                            //Remember this page is on its side, so values are not always easy to keep track of
+                            // 20-ylocation
+                            //      This is affecting the left hand side of the page, moving the image on the X Axis
+                            //      We moved the image as far to the left as possible, but leave a margin of 20
+                            // pageHeight-(pageHeight-xlocation)+5
+                            //      In order for the image to be in its original location, this value should be 595 == pageHeight
+                            //      We want to move the image to the bottom of the page, on the Y Axis so:
+                            //      We calculate the distance between the bottom of the image and the bottom of the page == (pageHeight-xlocation)
+                            //      We then move the image down by this amount, leaving an addition 5 for margin
+                            writer.DirectContent.AddTemplate(importedPage, 0, -1f, 1f, 0, 15-ylocation, pageHeight-(pageHeight-xlocation)+15);
+                            break;
+
+                        default:
+                            throw new InvalidOperationException(string.Format("Unexpected page rotation: [{0}].", pageRotation));
+                    }
+
+                    writer.CloseStream = false;
+                    document.Close();
+                    Console.WriteLine(pageRotation + "\t" + imgWidth + "\t" + imgHeight + "\t" + xlocation + "\t" + ylocation);
+                }
+            }
+
+            outputStream.Position = 0;
+            return outputStream;
+        }
+
+        public MemoryStream AddVARLicenceTracings(MemoryStream inputStream, float topMargin)
+        {
+            //Reproduced from Admiralty digital Notices to Mariners by permission of the Controller of Her Majesty’s Stationery 
+            //Office and the UK Hydrographic Office
+            //HO 2484/170721/01
+            PdfReader reader = new PdfReader((byte[])inputStream.ToArray());
+
+            MemoryStream outputStream = new MemoryStream();
+
+            using (Document document = new Document(reader.GetPageSizeWithRotation(1), 0, 0, 0, 0))
+            {
+                using (PdfWriter writer = PdfWriter.GetInstance(document, outputStream))
+                {
+                    document.Open();
+
+                    PdfImportedPage importedPage = writer.GetImportedPage(reader, 1);
+
+                    var pageRotation = reader.GetPageRotation(1);
+                    var pageWidth = reader.GetPageSizeWithRotation(1).Width;
+                    var pageHeight = reader.GetPageSizeWithRotation(1).Height;
+                    var titlefont = FontFactory.GetFont(BaseFont.COURIER, 7, Font.NORMAL);
+                    var title1 = new Paragraph(20, "Reproduced from Admiralty digital Notices to Mariners by permission of the Controller of Her Majesty’s Stationery", titlefont);
+                    var title2 = new Paragraph("Office and the UK Hydrographic Office", titlefont);
+                    var title3 = new Paragraph("HO " + UKHOVARLicenceNumber, titlefont);
+                    title1.Alignment = Element.ALIGN_RIGHT;
+                    title2.Alignment = Element.ALIGN_RIGHT;
+                    title3.Alignment = Element.ALIGN_RIGHT;
+                    title1.IndentationRight = 15;
+                    title2.IndentationRight = 15;
+                    title3.IndentationRight = 15;
+
+                    switch (pageRotation)
+                    {
+                        case 0:
+                            document.Add(title1);
+                            document.Add(title2);
+                            document.Add(title3);
+                            writer.DirectContent.AddTemplate(importedPage, 1f, 0, 0, 1f, 0, -topMargin);
+                            break;
+
+                        default:
+                            throw new InvalidOperationException(string.Format("Unexpected page rotation: [{0}].", pageRotation));
+                    }
+
+                    writer.CloseStream = false;
+                    document.Close();
+                }
+            }
+
+            outputStream.Position = 0;
+            return outputStream;
+        }
+
+        public MemoryStream AddVARLicenceWKNM(MemoryStream inputStream)
+        {
+            //Reproduced from Admiralty digital Notices to Mariners by permission of the Controller of Her Majesty’s Stationery 
+            //Office and the UK Hydrographic Office
+            //HO 2484/170721/01
+            PdfReader reader = new PdfReader((byte[])inputStream.ToArray());
+
+            MemoryStream outputStream = new MemoryStream();
+
+            using (Document document = new Document(reader.GetPageSizeWithRotation(1), 0, 0, 0, 0))
+            {
+                using (PdfWriter writer = PdfWriter.GetInstance(document, outputStream))
+                {
+                    document.Open();
+                    for (int pageNumber = 1; pageNumber < reader.NumberOfPages + 1; pageNumber++)
+                    {
+                        document.NewPage();
+                        PdfImportedPage importedPage = writer.GetImportedPage(reader, pageNumber);
+
+                        if (pageNumber == 1)
+                        {
+                            var pageRotation = reader.GetPageRotation(1);
+                            var pageWidth = reader.GetPageSizeWithRotation(1).Width;
+                            var pageHeight = reader.GetPageSizeWithRotation(1).Height;
+                            var titlefont = FontFactory.GetFont(BaseFont.COURIER, 7, Font.NORMAL);
+                            var title1 = new Paragraph(20, "HO " + UKHOVARLicenceNumber, titlefont);
+                            var title2 = new Paragraph(785, "Reproduced from Admiralty digital Notices to Mariners by permission of the ", titlefont);
+                            var title3 = new Paragraph("Controller of Her Majesty’s Stationery Office and the UK Hydrographic Office", titlefont);
+
+                            title1.Alignment = Element.ALIGN_RIGHT;
+                            title2.Alignment = Element.ALIGN_LEFT;
+                            title3.Alignment = Element.ALIGN_LEFT;
+                            title1.IndentationRight = 15;
+                            title2.IndentationLeft = 15;
+                            title3.IndentationLeft = 15;
+
+                            switch (pageRotation)
+                            {
+                                case 0:
+                                    document.Add(title1);
+                                    document.Add(title2);
+                                    document.Add(title3);
+                                    writer.DirectContent.AddTemplate(importedPage, 1f, 0, 0, 1f, 0, 0);
+                                    break;
+
+                                default:
+                                    throw new InvalidOperationException(string.Format("Unexpected page rotation: [{0}].", pageRotation));
+                            }
+                        }
+                        else
+                        {
+                            writer.DirectContent.AddTemplate(importedPage, 1f, 0, 0, 1f, 0, 0);
+                        }
+                    }
+
+                    writer.CloseStream = false;
+                    document.Close();
+                }
+            }
+
+            outputStream.Position = 0;
+            return outputStream;
+        }
+
+        public MemoryStream AddVARLicencePublicationCorrection(Stream inputStream)
+        {
+            //Reproduced from Admiralty digital Notices to Mariners by permission of the Controller of Her Majesty’s Stationery 
+            //Office and the UK Hydrographic Office
+            //HO 2484/170721/01
+            PdfReader reader = new PdfReader(inputStream);
+
+            MemoryStream outputStream = new MemoryStream();
+
+            using (Document document = new Document(reader.GetPageSizeWithRotation(1), 0, 0, 0, 0))
+            {
+                using (PdfWriter writer = PdfWriter.GetInstance(document, outputStream))
+                {
+                    document.Open();
+                    for (int pageNumber = 1; pageNumber < reader.NumberOfPages + 1; pageNumber++)
+                    {
+                        document.NewPage();
+                        PdfImportedPage importedPage = writer.GetImportedPage(reader, pageNumber);
+
+                        if (pageNumber == 1)
+                        {
+                            var pageRotation = reader.GetPageRotation(1);
+                            var pageWidth = reader.GetPageSizeWithRotation(1).Width;
+                            var pageHeight = reader.GetPageSizeWithRotation(1).Height;
+                            var titlefont = FontFactory.GetFont(BaseFont.COURIER, 7, Font.NORMAL);
+                            var title1 = new Paragraph(20, "HO " + UKHOVARLicenceNumber, titlefont);
+                            var title2 = new Paragraph(785, "Reproduced from Admiralty digital Notices to Mariners by permission of the ", titlefont);
+                            var title3 = new Paragraph("Controller of Her Majesty’s Stationery Office and the UK Hydrographic Office", titlefont);
+
+                            title1.Alignment = Element.ALIGN_RIGHT;
+                            title2.Alignment = Element.ALIGN_LEFT;
+                            title3.Alignment = Element.ALIGN_LEFT;
+                            title1.IndentationRight = 15;
+                            title2.IndentationLeft = 15;
+                            title3.IndentationLeft = 15;
+
+                            switch (pageRotation)
+                            {
+                                case 0:
+                                    document.Add(title1);
+                                    document.Add(title2);
+                                    document.Add(title3);
+                                    writer.DirectContent.AddTemplate(importedPage, 1f, 0, 0, 1f, 0, 0);
+                                    break;
+
+                                default:
+                                    throw new InvalidOperationException(string.Format("Unexpected page rotation: [{0}].", pageRotation));
+                            }
+                        }
+                        else
+                        {
+                            writer.DirectContent.AddTemplate(importedPage, 1f, 0, 0, 1f, 0, 0);
+                        }
+                    }
+
+                    writer.CloseStream = false;
+                    document.Close();
+                }
+            }
+
+            outputStream.Position = 0;
+            return outputStream;
+        }
+    }
+}
+
+
